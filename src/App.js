df7d784b19233598f1f37463c14c55ccd6014da5
eugene huang <eugene@goforward.com>
2023-08-27 13:36:28 -0700

    style font. uppercase project name

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function Projects() {
       <div className="mt-4">As a resolution in 2022, I started tinkering with personal projects.</div>
       {projects && projects.groups.map((group, i) =>
         <div>
-          <div className="mt-4">{group.name}</div>
+          <div className="mt-4">{group.name.toUpperCase()}</div>
           {group.repos && group.repos.map((repo, j) =>
             <div>
               <span className="font-bold">❧ <a href={`https://github.com/elh/`+repo.repo} className="link link-hover">{repo.repo} </a></span>
@@ -54,7 +54,7 @@ function App() {
       <div className="flex justify-center items-center pt-[4rem]">
         <div className="w-[40rem]">
           <header className="flex justify-between items-center">
-            <a href={process.env.PUBLIC_URL} className="text-2xl font-extrabold link link-hover">Eugene L Huang</a>
+            <a href={process.env.PUBLIC_URL} className="text-2xl font-extrabold link link-hover">{"Eugene L Huang".toUpperCase()}</a>
             <div className="flex space-x-2">
               <a href={`https://github.com/elh`} className="link link-hover"><Github size={16} strokeWidth={2.0} /></a>
               <a href={`https://www.linkedin.com/in/elhonline/`} className="link link-hover"><Linkedin size={16} strokeWidth={2.0} /></a>
