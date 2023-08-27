2868353770e84658a7660f57bc519e3ae9d01b20
eugene huang <eugene@goforward.com>
2023-08-27 01:17:39 -0700

    tidy up

--- src/App.js
+++ src/App.js
@@ -19,29 +19,21 @@ function Markdown({ fileName }) {
 
 /*
 TODO:
-fix vertical and horizontal sizing. why is there overflow
 generate the list from config file
 */
-
 function App() {
   return (
     <div>
-      <div className="flex justify-center h-screen pt-[4rem]">
-        <div className="w-[50rem]">
+      <div className="flex justify-center items-center pt-[4rem]">
+        <div className="w-[40rem]">
           <header className="flex justify-between items-center">
-            <a href={process.env.PUBLIC_URL} className="text-xl font-extrabold">Eugene L Huang</a>
-            <div className="flex space-x-4">
+            <a href={process.env.PUBLIC_URL} className="text-2xl font-extrabold link link-hover">Eugene L Huang</a>
+            <div className="flex space-x-2">
               <a href={`https://github.com/elh`} className="link link-hover"><Github size={16} strokeWidth={2.0} /></a>
               <a href={`https://www.linkedin.com/in/elhonline/`} className="link link-hover"><Linkedin size={16} strokeWidth={2.0} /></a>
               <a href={`https://twitter.com/elh_online`} className="link link-hover"><Twitter size={16} strokeWidth={2.0} /></a>
             </div>
           </header>
-          {/* <div className="text-sm breadcrumbs py-1">
-            <ul>
-              <li><a>/</a></li>
-              <li><a>projects</a></li>
-            </ul>
-          </div> */}
           <div className="mt-4">As a resolution in 2022, I started tinkering with side projects.</div>
           <div>
             <div className="mt-4">Logic Programming</div>
@@ -64,10 +56,6 @@ function App() {
               <span className="font-bold">❧ <a href={`https://github.com/elh/bitempura`} className="link link-hover">bitempura</a></span>
               <span> - Toy, in-memory, bitemporal key-value db</span>
             </div>
-            {/* <div>
-              <span className="font-bold">❧ <a href={`https://github.com/elh/bitempura-viz`} className="link link-hover">bitempura-viz</a></span>
-              <span> - Bitemporal 2D valid + tx time visualizer</span><a href={`https://elh.github.io/bitempura-viz/#/bitempura-viz/`} className="link link-hover"><ArrowUpRightSquare size={11} strokeWidth={2.0} />live</a>
-            </div> */}
             <div className="flex items-center space-x-2">
               <span className="font-bold">❧ <a href={`https://github.com/elh/bitempura-viz`} className="link link-hover">bitempura-viz</a></span>
               <span>
@@ -78,21 +66,8 @@ function App() {
               <span> - Bitemporal 2D valid + tx time visualizer</span>
             </div>
           </div>
-          {/* <ul class="list-none">
-            <li>
-              <span className="font-bold">❧ whodunit</span>
-              <span> - Logic puzzle generator using core.logic</span>
-            </li>
-          </ul> */}
-          {/* <div class="chat chat-start">
-            <div class="chat-bubble">Whats up</div>
-            <div class="chat-footer opacity-70">
-              Eugene Huang
-            </div>
-          </div> */}
         </div>
       </div>
-      {/* <Markdown fileName={"readmes/whodunit.md"} /> */}
     </div>
   );
 }
