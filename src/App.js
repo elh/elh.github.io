ce2cfdf154b46764c2fa907063541b575b173f88
eugene huang <eugene@goforward.com>
2023-08-28 20:29:05 -0700

    toggle project desc and question on click

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown'
-import { Github, Linkedin, Twitter } from 'lucide-react';
+import { Github, Linkedin, Twitter, MousePointerClick } from 'lucide-react';
 import coverImg from "./img/cover.jpeg"; // h/t https://unsplash.com/photos/KgOpmX1STew
 
 // note: use this to bootstrap project specific pager later
@@ -22,6 +22,12 @@ function Markdown({ fileName }) {
 /* eslint-disable no-unused-vars */
 
 function Projects({ projects }) {
+  const [showQuestions, setShowQuestions] = useState(false);
+
+  const toggleShowQuestions = () => {
+    setShowQuestions(!showQuestions);
+  };
+
   if (!projects) {
     return null;
   }
@@ -29,7 +35,7 @@ function Projects({ projects }) {
     <div>
       <div className="mt-4">
         {/* Factor out into content file when I support html/markdown */}
-        Since <a href={"https://elh.github.io/gh-organizer/#/owners/elh/repo-timeline"} className="link">2022</a>, I started tinkering with personal projects as a resolution to share my thoughts more. I use these weeklong spikes to learn-by-doing and be <span className="underline decoration-wavy underline-offset-0 decoration-2 decoration-emerald-600">creative</span>. All projects are functional MVPs, documented, and runnable. Check them out!
+        Since <a href={"https://elh.github.io/gh-organizer/#/owners/elh/repo-timeline"} className="link">2022</a>, I started tinkering with personal projects as a resolution to share my thoughts more. I use these weeklong spikes to <a className="link link-hover" onClick={toggleShowQuestions}>learn-by-doing<MousePointerClick size={16} strokeWidth={1.6} /></a> and be <span className="underline decoration-wavy underline-offset-0 decoration-2 decoration-emerald-600">creative</span>. All projects are functional MVPs, documented, and runnable. Check them out!
       </div>
       <div className="my-4 text-center text-xl">⁂</div>
       {projects && projects.groups.map((group, i) =>
@@ -41,17 +47,20 @@ function Projects({ projects }) {
                 ? <span>
                   <span className="font-bold">❧ </span>
                   {repo.repos.map((repo, k) =>
-                    <span className="font-bold"><a href={`https://github.com/elh/`+repo} className="link link-hover">{repo}{k > 0 ? " ": ", "}</a></span>
+                    <span className="font-bold"><a href={`https://github.com/elh/`+repo} className="link link-hover decoration-2">{repo}{k > 0 ? " ": ", "}</a></span>
                   )}
                   </span>
-                : <span className="font-bold">❧ <a href={`https://github.com/elh/`+repo.repo} className="link link-hover">{repo.repo} </a></span>
+                : <span className="font-bold">❧ <a href={`https://github.com/elh/`+repo.repo} className="link link-hover decoration-2">{repo.repo} </a></span>
               }
               { repo.homepage &&
                 <span>
                   (<a href={repo.homepage} className="link">site</a>)
                 </span>
               }
-              <span> - {repo.desc}</span>
+              { showQuestions
+                ? <span> - {repo.q}</span>
+                : <span> - {repo.desc}</span>
+              }
             </div>
           )}
         </div>
@@ -71,10 +80,10 @@ function App() {
   return (
     <div>
       <div className="flex justify-center">
-        <div className="w-[44rem] mx-4 mb-4">
+        <div className="w-[44rem] mx-4 mb-6">
           <img className="mb-6 invert-0 dark:invert" alt="" src={coverImg} />
           <header className="flex justify-between items-center">
-            <a href={process.env.PUBLIC_URL} className="text-2xl font-extrabold link link-hover">{"Eugene L Huang".toUpperCase()}</a>
+            <a href={process.env.PUBLIC_URL} className="text-2xl font-extrabold link link-hover decoration-4">{"Eugene L Huang".toUpperCase()}</a>
             <div className="flex space-x-2">
               <a href={`https://github.com/elh`} className="link link-hover"><Github size={16} strokeWidth={2.0} /></a>
               <a href={`https://www.linkedin.com/in/elhonline/`} className="link link-hover"><Linkedin size={16} strokeWidth={2.0} /></a>
