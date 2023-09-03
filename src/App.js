a2638e9c840b6d4cf8c1bd1ee24287d19f5d7bfd
eugene huang <eugene@goforward.com>
2023-09-02 17:19:16 -0700

    fidget with spacing

--- src/App.js
+++ src/App.js
@@ -37,26 +37,26 @@ function Projects({ projects, iframed }) {
     <div>
       <div className="mt-4">
         {/* Factor out into content file when I support html/markdown */}
-        Since <a href={"https://elh.github.io/gh-organizer/#/owners/elh/repo-timeline"} rel="noreferrer" className="link">2022</a>, I started tinkering with personal projects as a resolution to share my thoughts more. I use these weeklong spikes to <button className="link" onClick={toggleShowQuestions}>learn-by-doing<MousePointerClick size={16} strokeWidth={1.6} /></button> and be {isMobile ? <button className="tooltip" data-tip="Desktop only"><span className="underline decoration-wavy underline-offset-0 decoration-2 decoration-emerald-600">creative</span><MousePointerClick size={16} strokeWidth={1.6} /></button> : <a href={"https://elh.github.io/ePhone?url=https://elh.github.io/&id=" + ePhoneID}><span className="underline decoration-wavy underline-offset-0 decoration-2 decoration-emerald-600">creative</span><MousePointerClick size={16} strokeWidth={1.6} /></a>}. All projects are functional MVPs, documented, and runnable. Check them out!
+        Since <a href={"https://elh.github.io/gh-organizer/#/owners/elh/repo-timeline"} rel="noreferrer" className="link">2022</a>, I started tinkering with personal projects as a resolution to share my thoughts more. I use these weeklong spikes to <button className="link decoration-2" onClick={toggleShowQuestions}>learn-by-doing<MousePointerClick size={16} strokeWidth={1.6} /></button> and be {isMobile ? <button className="tooltip" data-tip="Desktop only"><span className="underline decoration-wavy underline-offset-0 decoration-2 decoration-emerald-600">creative</span><MousePointerClick size={16} strokeWidth={1.6} /></button> : <a href={"https://elh.github.io/ePhone?url=https://elh.github.io/&id=" + ePhoneID}><span className="underline decoration-wavy underline-offset-0 decoration-2 decoration-emerald-600">creative</span><MousePointerClick size={16} strokeWidth={1.6} /></a>}. All projects are functional MVPs, documented, and runnable. Check them out!
       </div>
-      <div className="mt-4 text-center text-xl">⁂</div>
+      {/* <div className="mt-4 text-center text-xl">⁂</div> */}
       {projects && projects.groups.map((group, i) =>
-        <div className={i > 0 ? "mt-4" : ""}>
-          <div><span className="text-2xl">❧</span> {group.name.toUpperCase()}</div>
+        <div className={i > 0 ? "mt-3" : "mt-6"}>
+          <div className='font-bold'><span className="text-xl">❧</span> {group.name.toUpperCase()}</div>
           {group.repos && group.repos.map((repo, j) =>
             <div>
               { repo.repos
                 ? <span>
-                  <span className="font-bold"></span>
+                  <span className=""></span>
                   {repo.repos.map((repo, k) =>
-                    <span className="font-bold"><a href={`https://github.com/elh/`+repo} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover decoration-2">{repo}{k > 0 ? " ": ", "}</a></span>
+                    <span className=""><a href={`https://github.com/elh/`+repo} target={iframed ? "_blank": ""} rel="noreferrer" className="link">{repo}</a>{k > 0 ? "": ","}&nbsp;</span>
                   )}
                   </span>
-                : <span className="font-bold"><a href={`https://github.com/elh/`+repo.repo} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover decoration-2">{repo.repo} </a></span>
+                : <span className=""><a href={`https://github.com/elh/`+repo.repo} target={iframed ? "_blank": ""} rel="noreferrer" className="link">{repo.repo}</a></span>
               }
               { repo.homepage &&
                 <span>
-                  (<a href={repo.homepage} target={iframed && !repo.iframe_safe ? "_blank": ""} rel="noreferrer" className="link">site</a>)
+                  &nbsp;(<a href={repo.homepage} target={iframed && !repo.iframe_safe ? "_blank": ""} rel="noreferrer" className="link">site</a>)
                 </span>
               }
               { showQuestions
@@ -88,13 +88,13 @@ function App() {
           <header className="flex justify-between items-center mt-6">
             <a href={process.env.PUBLIC_URL} className="text-5xl font-black link link-hover decoration-4">{"Eugene L Huang".toUpperCase()}</a>
             <div className="flex space-x-2">
-              <a href={`https://github.com/elh`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover"><Github size={24} strokeWidth={2.4} /></a>
-              <a href={`https://www.linkedin.com/in/elhonline/`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover"><Linkedin size={24} strokeWidth={2.4} /></a>
-              <a href={`https://twitter.com/elh_online`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover"><Twitter size={24} strokeWidth={2.4} /></a>
+              <a href={`https://github.com/elh`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover"><Github size={24} strokeWidth={2.2} /></a>
+              <a href={`https://www.linkedin.com/in/elhonline/`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover"><Linkedin size={24} strokeWidth={2.2} /></a>
+              <a href={`https://twitter.com/elh_online`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover"><Twitter size={24} strokeWidth={2.2} /></a>
             </div>
           </header>
           <Projects projects={content.projects} iframed={iframed} />
-          <div className="my-4 text-center text-xl">⁂</div>
+          <div className="my-6 text-center text-xl">⁂</div>
           {/* Factor out into content file when I support html/markdown */}
           <div className="mt-4">I'm an engineer at <a href={`https://goforward.com/technology`} target={iframed ? "_blank": ""} rel="noreferrer" className="link">Forward</a> working on radically rebuilding healthcare in software and hardware. Previously, I was building a new data management product at Box and studied EECS at UC Berkeley. :)</div>
         </div>
