ffd2512b657a934150196fb718e8f687a075da92
eugene huang <eugene@goforward.com>
2024-03-28 23:09:42 -0700

    make images a lil punchy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,11 +12,23 @@ import carepod from "../img/carepod.png";
 import elh from "../img/elh.png";
 import Torus from '../components/Torus';
 
-function Absolute({ children, style }) {
+function Absolute({ children, style, className }) {
+  if (className === undefined) {
+    className = "";
+  }
   return (
-    <div className='absolute' style={style}>
+    <div className={`absolute ${className}`} style={style}>
       {children}
     </div>
+  );
+}
+
+function Image({ src, alt, className }) {
+  if (className === undefined) {
+    className = "";
+  }
+  return (
+    <img className={`grayscale hover:grayscale-0 relative transition-transform duration-100 transform hover:scale-[1.02] ${className}`} src={src} alt={alt} />
   );
 }
 
@@ -26,10 +38,22 @@ function Home({ iframed }) {
       <div className='relative max-w-screen-md mx-auto'>
         {!isMobile && <Torus color="#b0b0b0" />} {/* no 3D on mobile */}
 
+        <div className="mt-8 flex flex-wrap space-x-2 justify-end">
+          <a href={`https://github.com/elh`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover" aria-label="Github">
+            <Github size={20} strokeWidth={1.7} alt="Github" />
+          </a>
+          <a href={`https://www.linkedin.com/in/elhonline/`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover" aria-label="Linkedin">
+            <Linkedin size={20} strokeWidth={1.7} alt="LinkedIn" />
+          </a>
+          <a href={`https://twitter.com/elh_online`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover" aria-label="Twitter">
+            <Twitter size={20} strokeWidth={1.7} alt="Twitter" />
+          </a>
+        </div>
+
         {/* Absolute positioning */}
         <Absolute style={{top: 30}}>
           <header className="flex flex-wrap justify-between items-center mt-6 mb-4">
-            <h1 className="text-6xl uppercase mobile-header">
+            <h1 className="text-6xl uppercase mobile-header text-green-700">
               <span className='fleuronregular'>E</span>
               <span className='fleuronmixed'>u</span>
               <span className='fleuronregular'>g</span>
@@ -57,54 +81,42 @@ function Home({ iframed }) {
 
         <Absolute style={{top: 330, left:10, width:"14%"}}>
           <a className='block' href={`https://letterboxd.com/film/chungking-express/`} target={iframed ? "_blank": ""} rel="noreferrer" aria-label="Letterboxd - Chungking Express">
-            <img className='grayscale hover:grayscale-0 z-40 relative nomobile' src={chungkingExpress} alt="Chungking Express" />
+            <Image src={chungkingExpress} alt="Chungking Express" className="z-40 nomobile" />
           </a>
           </Absolute>
         <Absolute style={{top: 280, left:110, width:"14%"}}>
           <a className='block' href={`https://letterboxd.com/film/mishima-a-life-in-four-chapters/`} target={iframed ? "_blank": ""} rel="noreferrer" aria-label="Letterboxd - Mishima: A Life in Four Chapters">
-            <img className='grayscale hover:grayscale-0 z-30 relative nomobile' src={mishima} alt="Mishima: A Life in Four Chapters" />
+            <Image src={mishima} alt="Mishima: A Life in Four Chapters" className="z-30 nomobile" />
           </a>
           </Absolute>
         <Absolute style={{top: 355, left:200, width:"14%"}}>
           <a className='block' href={`https://letterboxd.com/film/the-matrix/`} target={iframed ? "_blank": ""} rel="noreferrer" aria-label="Letterboxd - The Matrix">
-            <img className='grayscale hover:grayscale-0 z-20 relative nomobile' src={theMatrix} alt="The Matrix" />
+            <Image src={theMatrix} alt="The Matrix" className="z-20 nomobile" />
           </a>
         </Absolute>
         <Absolute style={{top: 300, left:285, width:"14%"}}>
           <a className='block' href={`https://letterboxd.com/film/before-sunset/`} target={iframed ? "_blank": ""} rel="noreferrer" aria-label="Letterboxd - Before Sunset">
-            <img className='grayscale hover:grayscale-0 z-10 relative nomobile' src={beforeSunset} alt="Before Sunset" />
+            <Image src={beforeSunset} alt="Before Sunset" className="z-10 nomobile" />
           </a>
         </Absolute>
 
         <Absolute style={{top: 530, left:10, width:"26%"}}>
           <a href={`https://www.instagram.com/eugeually/`} target={iframed ? "_blank": ""} rel="noreferrer" aria-label="Instagram">
-            <img className='grayscale hover:grayscale-0 z-100 relative nomobile' src={perfectDays} alt="Perfect Days" />
+            <Image src={perfectDays} alt="Perfect Days" className="nomobile" />
           </a>
         </Absolute>
 
         <Absolute style={{top: 50, right:20, width:"30%"}}>
-          <a href={`https://goforward.com/carepod`} target={iframed ? "_blank": ""} rel="noreferrer" aria-label="Instagram">
-            <img className='grayscale hover:grayscale-0 z-100 relative nomobile' src={carepod} alt="Forward CarePod" />
+          <a href={`https://goforward.com/carepod`} target={iframed ? "_blank": ""} rel="noreferrer" aria-label="Forward CarePod">
+            <Image src={carepod} alt="Forward CarePod" className="nomobile" />
           </a>
         </Absolute>
 
         <Absolute style={{top: 580, left:250, width:"16%"}}>
           <a href={`https://github.com/elh`} target={iframed ? "_blank": ""} rel="noreferrer" aria-label="Github">
-            <img className='grayscale hover:grayscale-0 z-100 relative nomobile' src={elh} alt="Eugene" />
+            <Image src={elh} alt="Eugene" className="nomobile" />
           </a>
         </Absolute>
-
-        <div className="mt-8 flex flex-wrap space-x-2 justify-end">
-          <a href={`https://github.com/elh`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover" aria-label="Github">
-            <Github size={20} strokeWidth={1.7} alt="Github" />
-          </a>
-          <a href={`https://www.linkedin.com/in/elhonline/`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover" aria-label="Linkedin">
-            <Linkedin size={20} strokeWidth={1.7} alt="LinkedIn" />
-          </a>
-          <a href={`https://twitter.com/elh_online`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover" aria-label="Twitter">
-            <Twitter size={20} strokeWidth={1.7} alt="Twitter" />
-          </a>
-        </div>
       </div>
     </div>
   );
