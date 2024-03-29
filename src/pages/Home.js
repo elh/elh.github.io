7c0f8129c77668594eb451e3b2a7d7274c3374dd
eugene huang <eugene@goforward.com>
2024-03-28 22:37:51 -0700

    move social links. adjust vertical spacing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,8 +27,8 @@ function Home({ iframed }) {
         {!isMobile && <Torus color="#b0b0b0" />} {/* no 3D on mobile */}
 
         {/* Absolute positioning */}
-        <Absolute style={{top: 50}}>
-          <header className="flex flex-wrap justify-between items-center mt-6 mb-8">
+        <Absolute style={{top: 30}}>
+          <header className="flex flex-wrap justify-between items-center mt-6 mb-4">
             <h1 className="text-6xl uppercase mobile-header">
               <span className='fleuronregular'>E</span>
               <span className='fleuronmixed'>u</span>
@@ -41,10 +41,7 @@ function Home({ iframed }) {
               <span className='fleuronmixed'>g</span>
             </h1>
           </header>
-        </Absolute>
-
-        <Absolute style={{top: 130, left: 14}}>
-          <ul>
+          <ul className="ml-8">
             <li>makes <a href={`https://goforward.com/carepod`} target={iframed ? "_blank": ""} rel="noreferrer" className="link decoration-2 hover:text-emerald-600">crazy healthtech</a> @ Forward in SF</li>
             <li>hacks on <a href={process.env.PUBLIC_URL+`#/projects`} rel="noreferrer" className="link decoration-2 hover:text-emerald-600">pet projects</a> at night</li>
             <li>devoutly logs movies on <a href={"https://letterboxd.com/eugeually/"} rel="noreferrer" className="link decoration-2 hover:text-emerald-600">Letterboxd</a></li>
@@ -53,18 +50,9 @@ function Home({ iframed }) {
         </Absolute>
 
         <Absolute style={{top: 240, right: 0, width:"45%"}}>
+          <span className="font-serif	italic">Figure 1. me</span>
+          {/* TODO: replace this with ePhone */}
           <img src={selfieImg} alt='Eugene'/>
-          <div className="my-4 flex flex-wrap space-x-2 justify-end">
-            <a href={`https://github.com/elh`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover" aria-label="Github">
-              <Github size={20} strokeWidth={1.7} alt="Github" />
-            </a>
-            <a href={`https://www.linkedin.com/in/elhonline/`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover" aria-label="Linkedin">
-              <Linkedin size={20} strokeWidth={1.7} alt="LinkedIn" />
-            </a>
-            <a href={`https://twitter.com/elh_online`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover" aria-label="Twitter">
-              <Twitter size={20} strokeWidth={1.7} alt="Twitter" />
-            </a>
-          </div>
         </Absolute>
 
         <Absolute style={{top: 330, left:10, width:"14%"}}>
@@ -88,7 +76,7 @@ function Home({ iframed }) {
           </a>
         </Absolute>
 
-        <Absolute style={{top: 510, left:-20, width:"30%"}}>
+        <Absolute style={{top: 530, left:10, width:"26%"}}>
           <a href={`https://www.instagram.com/eugeually/`} target={iframed ? "_blank": ""} rel="noreferrer" aria-label="Instagram">
             <img className='grayscale hover:grayscale-0 z-100 relative nomobile' src={perfectDays} alt="Perfect Days" />
           </a>
@@ -105,6 +93,18 @@ function Home({ iframed }) {
             <img className='grayscale hover:grayscale-0 z-100 relative nomobile' src={elh} alt="Eugene" />
           </a>
         </Absolute>
+
+        <div className="mt-8 flex flex-wrap space-x-2 justify-end">
+          <a href={`https://github.com/elh`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover" aria-label="Github">
+            <Github size={20} strokeWidth={1.7} alt="Github" />
+          </a>
+          <a href={`https://www.linkedin.com/in/elhonline/`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover" aria-label="Linkedin">
+            <Linkedin size={20} strokeWidth={1.7} alt="LinkedIn" />
+          </a>
+          <a href={`https://twitter.com/elh_online`} target={iframed ? "_blank": ""} rel="noreferrer" className="link link-hover" aria-label="Twitter">
+            <Twitter size={20} strokeWidth={1.7} alt="Twitter" />
+          </a>
+        </div>
       </div>
     </div>
   );
